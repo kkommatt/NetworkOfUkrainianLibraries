30b68c066345aa6d3a09a9622e9bc66f14d4d932
Matthew Kovalyk <matviigame@gmail.com>
2023-02-28 00:08:06 +0200

    Something

---
+++ NetworkOfLibrariesWebApplication/Controllers/LibrariesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NetworkOfLibrariesWebApplication;
+
+namespace NetworkOfLibrariesWebApplication.Controllers
+{
+    public class LibrariesController : Controller
+    {
+        private readonly DbnetworkOfLibrariesContext _context;
+
+        public LibrariesController(DbnetworkOfLibrariesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Libraries
+        public async Task<IActionResult> Index()
+        {
+            var dbnetworkOfLibrariesContext = _context.Libraries.Include(l => l.City);
+            return View(await dbnetworkOfLibrariesContext.ToListAsync());
+        }
+
+        // GET: Libraries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Libraries == null)
+            {
+                return NotFound();
+            }
+
+            var library = await _context.Libraries
+                .Include(l => l.City)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (library == null)
+            {
+                return NotFound();
+            }
+
+            return View(library);
+        }
+
+        // GET: Libraries/Create
+        public IActionResult Create()
+        {
+            ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Id");
+            return View();
+        }
+
+        // POST: Libraries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Adress,Website,Schedule,CityId")] Library library)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(library);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Id", library.CityId);
+            return View(library);
+        }
+
+        // GET: Libraries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Libraries == null)
+            {
+                return NotFound();
+            }
+
+            var library = await _context.Libraries.FindAsync(id);
+            if (library == null)
+            {
+                return NotFound();
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Id", library.CityId);
+            return View(library);
+        }
+
+        // POST: Libraries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Adress,Website,Schedule,CityId")] Library library)
+        {
+            if (id != library.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(library);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LibraryExists(library.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Id", library.CityId);
+            return View(library);
+        }
+
+        // GET: Libraries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Libraries == null)
+            {
+                return NotFound();
+            }
+
+            var library = await _context.Libraries
+                .Include(l => l.City)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (library == null)
+            {
+                return NotFound();
+            }
+
+            return View(library);
+        }
+
+        // POST: Libraries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Libraries == null)
+            {
+                return Problem("Entity set 'DbnetworkOfLibrariesContext.Libraries'  is null.");
+            }
+            var library = await _context.Libraries.FindAsync(id);
+            if (library != null)
+            {
+                _context.Libraries.Remove(library);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LibraryExists(int id)
+        {
+          return (_context.Libraries?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- NetworkOfLibrariesWebApplication/Program.cs
+++ NetworkOfLibrariesWebApplication/Program.cs
@@ -1,7 +1,14 @@
+using NetworkOfLibrariesWebApplication;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbnetworkOfLibrariesContext>(option => option.UseSqlServer(
+ builder.Configuration.GetConnectionString("DefaultConnection")
+ ));
 
 var app = builder.Build();
 
@@ -22,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Libraries}/{action=Index}/{id?}");
 
 app.Run();
